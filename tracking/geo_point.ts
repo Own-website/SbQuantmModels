9e23dba2da2072a8170ea2010a13c338c770f20b
Shivam Tomar <54415001+Own-website@users.noreply.github.com>
2022-05-07 11:54:10 +0530

    Add files via upload

---
+++ tracking/geo_point.ts
@@ -0,0 +1,22 @@
+import {Schema , model, Types} from "mongoose"
+
+export class GeoPoint {
+    _id?:Types.ObjectId;
+    latitude_deg?: number ;
+    longitute_deg?: number;
+    altitude_deg?: number;
+}
+
+const GeoPointSchema: Schema =new Schema({
+    latitude_deg: {
+        type: Number
+    },
+    longitute_deg: {
+        type: Number
+    },
+    altitude_deg: {
+        type: Number
+    },
+}, {timestamps: true});
+
+export const GeoPointModel = model<GeoPoint>('geopoint',GeoPointSchema)
